b7b5e3562baff4fc133f36ce49f2d40ffeddba7d
Ananthakrishnan.S <ananthakrishnans.muhamma@gmail.com>
2022-01-23 16:14:45 +0530

    images anddescription added

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const randomPrice = Math.floor(Math.random() * 40 +10);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://picsum.photos/600',
+            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam expedita et iste assumenda sunt a fugiat doloribus veritatis dolores eius minima eaque animi dolorum non aperiam provident sit, quos quisquam.",
+            price: randomPrice
         })
         await camp.save();
     }
